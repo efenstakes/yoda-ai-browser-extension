9624e2ce86871791847343c16a95f68bfd85b4fc
Efen Stakes <efenstakes101@gmail.com>
2023-11-09 15:43:12 +0300

    show no prompts component when we have no prompts

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 // components
 import PromptCard from "./components/prompt/component"
 import Header from './components/header/component';
+import NoPrompts from './components/no_prompts/component';
 
 // services
 import { getPrompt, } from "./services/prompt"
@@ -12,44 +13,46 @@ import { getPrompt, } from "./services/prompt"
 // styles
 import './App.css';
 
+const testPrompts = [
+
+  {
+    prompt: "How are you?",
+    reply: "I dont know, I'm just an AI without feelings."
+  },
+  {
+    prompt: "Does that mean that there is an AI out there with feelings?",
+    reply: "I dont know that either, I would guess not from my data."
+  },
+  {
+    prompt: "How are you?",
+    reply: "I dont know, I'm just an AI without feelings."
+  },
+  {
+    prompt: "Does that mean that there is an AI out there with feelings?",
+    reply: "I dont know that either, I would guess not from my data."
+  },
+  {
+    prompt: "How are you?",
+    reply: "I dont know, I'm just an AI without feelings."
+  },
+  {
+    prompt: "Does that mean that there is an AI out there with feelings?",
+    reply: "I dont know that either, I would guess not from my data."
+  },
+  {
+    prompt: "How are you?",
+    reply: "I dont know, I'm just an AI without feelings."
+  },
+  {
+    prompt: "Does that mean that there is an AI out there with feelings?",
+    reply: "I dont know that either, I would guess not from my data."
+  },
+]
 function App() {
   const promptListRef = useRef()
   const [prompt, setPrompt] = useState('')
   const [isLoading, setIsLoading] = useState(false)
-  const [prompts, setPrompts] = useState([
-    {
-      prompt: "How are you?",
-      reply: "I dont know, I'm just an AI without feelings."
-    },
-    {
-      prompt: "Does that mean that there is an AI out there with feelings?",
-      reply: "I dont know that either, I would guess not from my data."
-    },
-    {
-      prompt: "How are you?",
-      reply: "I dont know, I'm just an AI without feelings."
-    },
-    {
-      prompt: "Does that mean that there is an AI out there with feelings?",
-      reply: "I dont know that either, I would guess not from my data."
-    },
-    {
-      prompt: "How are you?",
-      reply: "I dont know, I'm just an AI without feelings."
-    },
-    {
-      prompt: "Does that mean that there is an AI out there with feelings?",
-      reply: "I dont know that either, I would guess not from my data."
-    },
-    {
-      prompt: "How are you?",
-      reply: "I dont know, I'm just an AI without feelings."
-    },
-    {
-      prompt: "Does that mean that there is an AI out there with feelings?",
-      reply: "I dont know that either, I would guess not from my data."
-    },
-  ])
+  const [prompts, setPrompts] = useState([])
 
 
   const submitPrompt = async ()=> {
@@ -91,6 +94,8 @@ function App() {
         }
         <div className='prompts_list_pad'  ref={promptListRef} />
       </div>
+
+      { !prompts.length && <NoPrompts /> }
 
       {/* input */}
       <div className="row prompt_input">
