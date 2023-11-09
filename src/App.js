b9210f6230123b78fd8de0b960eb589ecdae98e3
Efen Stakes <efenstakes101@gmail.com>
2023-11-09 15:26:35 +0300

    add home page components and logic to get prompts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
+import { AiOutlineSend } from 'react-icons/ai'
+import CircularProgress from '@mui/material/CircularProgress';
+
+// components
+import PromptCard from "./components/prompt/component"
+import Header from './components/header/component';
+
+// services
+import { getPrompt, } from "./services/prompt"
+
+// styles
 import './App.css';
 
 function App() {
+  const promptListRef = useRef()
+  const [prompt, setPrompt] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [prompts, setPrompts] = useState([
+    {
+      prompt: "How are you?",
+      reply: "I dont know, I'm just an AI without feelings."
+    },
+    {
+      prompt: "Does that mean that there is an AI out there with feelings?",
+      reply: "I dont know that either, I would guess not from my data."
+    },
+    {
+      prompt: "How are you?",
+      reply: "I dont know, I'm just an AI without feelings."
+    },
+    {
+      prompt: "Does that mean that there is an AI out there with feelings?",
+      reply: "I dont know that either, I would guess not from my data."
+    },
+    {
+      prompt: "How are you?",
+      reply: "I dont know, I'm just an AI without feelings."
+    },
+    {
+      prompt: "Does that mean that there is an AI out there with feelings?",
+      reply: "I dont know that either, I would guess not from my data."
+    },
+    {
+      prompt: "How are you?",
+      reply: "I dont know, I'm just an AI without feelings."
+    },
+    {
+      prompt: "Does that mean that there is an AI out there with feelings?",
+      reply: "I dont know that either, I would guess not from my data."
+    },
+  ])
+
+
+  const submitPrompt = async ()=> {
+    console.log("submitPrompt called");
+    if( !prompt || isLoading ) return
+
+    setIsLoading(true)
+    const data = await getPrompt({ prompt, })
+
+    console.log('====================================');
+    console.log("data ", data);
+    console.log('====================================');
+    if( !data ) {
+      setIsLoading(false)
+    }
+
+    setPrompts([ ...prompts, { prompt, reply: data?.reply, } ])
+    setPrompt('')
+    setIsLoading(false)
+
+    promptListRef.current.scrollIntoView({ behavior: "smooth" });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* header */}
+      <Header />
+
+      {/* prompts */}
+      <div className='prompts_list'>
+        {
+          prompts.map((prompt, index)=> {
+
+            return (
+              <PromptCard key={index} prompt={prompt} />
+            )
+          })
+        }
+        <div className='prompts_list_pad'  ref={promptListRef} />
+      </div>
+
+      {/* input */}
+      <div className="row prompt_input">
+        <input
+          type="text"
+          className="prompt_input_input"
+          placeholder="Ask Yoda..."
+          value={prompt}
+          onChange={
+            (e)=> setPrompt(e.target.value)
+          }
+        />
+        <div className='prompt_input__icon' onClick={submitPrompt}>
+          { !isLoading && <AiOutlineSend size={20} /> }
+          { isLoading && <CircularProgress size={20} /> }
+        </div>
+      </div>
     </div>
   );
 }
